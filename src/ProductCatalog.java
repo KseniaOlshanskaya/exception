30deac44afe4267431475a82f245317d09a78058
XeniaOlshanskaya <ks14012000@gmail.com>
2020-09-23 21:30:53 +0700

    made 1st method

--- src/ProductCatalog.java
+++ src/ProductCatalog.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductCatalog {
+    ArrayList<Product> listOfProducts = new ArrayList<>();
 
     public ProductCatalog(){
+    }
 
+    public List<Product> getAllProducts() {
+        return this.listOfProducts;
     }
 
 }
