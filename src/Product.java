28a8d4ddfef090b32b5c7c8a6d539175293abab8
XeniaOlshanskaya <ks14012000@gmail.com>
2020-09-23 21:28:03 +0700

    Made ProductCatalog class

--- src/Product.java
+++ src/Product.java
@@ -3,4 +3,12 @@ public class Product {
     private String name;
     private double price;
     private String category;
+
+    public Product(long id, String name, double price, String category){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+
+    }
 }
